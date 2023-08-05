71845ac5ef950b9399901a28b8754d9d33246d47
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-08-05 22:31:16 +0600

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    Aircnc
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -0,0 +1,11 @@
+import avatarImg from '../../../assets/images/placeholder.jpg'
+
+const Avatar = () => {
+    return (
+        <div>
+            <img src={avatarImg} alt="" />
+        </div>
+    );
+};
+
+export default Avatar;
