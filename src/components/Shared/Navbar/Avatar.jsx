b402c99b1db3441087457d6213e38a631c6c9648
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-08-07 20:31:50 +0600

    AuthProvider

--- src/components/Shared/Navbar/Avatar.jsx
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -3,7 +3,7 @@ import avatarImg from '../../../assets/images/placeholder.jpg'
 const Avatar = () => {
     return (
         <div>
-            <img src={avatarImg} alt="" />
+            <img className='rounded-full' src={avatarImg} alt="profile" height="30" width="30" />
         </div>
     );
 };
