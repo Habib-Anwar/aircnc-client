f6e7da1fda708583b95140b70ff97cbe6cc90b7f
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-08-24 21:12:06 +0600

    Set Loader

--- src/components/Rooms/Rooms.jsx
+++ src/components/Rooms/Rooms.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Container from "../Shared/Container";
 import Card from "./Card";
+import Loader from "../Shared/Loader";
 
 
 const Rooms = () => {
@@ -15,9 +16,13 @@ const Rooms = () => {
     setLoading(false)})
      .catch(err => console.log(err))
     },[])
+
+    if(loading){
+        return <Loader></Loader>
+    }
     return (
         <Container>
-            <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">{rooms.map(room=><Card key={room.location}
+            <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">{rooms.map((room, i)=><Card key={i}
             room={room}></Card>)}
            
         </div>
