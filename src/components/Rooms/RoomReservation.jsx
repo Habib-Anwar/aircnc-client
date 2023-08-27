1367846053a0aa867f9aec3e073a9b09a9a20393
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-08-27 18:27:56 +0600

    calender setup

--- src/components/Rooms/RoomReservation.jsx
+++ src/components/Rooms/RoomReservation.jsx
@@ -1,4 +1,5 @@
-
+import Calender from '../Rooms/Calender'
+import Button from '../Button/Button'
 
 const RoomReservation = () => {
     return (
@@ -6,9 +7,19 @@ const RoomReservation = () => {
             <div className="flex flex-row items-center gap-1 p-4">
                 <div className="text-2xl font-semibold">$ 200</div>
                 <div className="font-light text-neutral-600">night</div>
-
             </div>
-            
+            <hr />
+            <div className='text-center'>
+
+            <Calender></Calender>
+            </div>
+            <hr />
+            <div className='p-4'><Button label="Reserve"></Button></div>
+            <hr />
+            <div className='p-4 flex flex-row items-center justify-between font-semibold text-lg'>
+             <div>Total</div>
+             <div>$ 300</div>
+            </div>
         </div>
     );
 };
